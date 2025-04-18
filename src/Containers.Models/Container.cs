f32967812614ccde5b60c2b750d6241c4a2cc03a
s31153 <s31153@pjwstk.edu.pl>
2025-04-18 16:21:05 +0200

    Created models

--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -2,5 +2,8 @@
 
 public class Container
 {
-    
+    public int ID { get; set; }
+    public int ContainerTypeId { get; set; }
+    public bool IsHazardous { get; set; }
+    public string Name { get; set; }
 }
